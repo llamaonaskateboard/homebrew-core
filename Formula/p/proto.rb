4bad98e0f1894f20f473d5d3e126b6d173322dd4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-03-21 00:51:58 +0000

    proto 0.32.0

--- Formula/p/proto.rb
+++ Formula/p/proto.rb
@@ -1,8 +1,8 @@
 class Proto < Formula
   desc "Pluggable multi-language version manager"
   homepage "https://moonrepo.dev/proto"
-  url "https://github.com/moonrepo/proto/archive/refs/tags/v0.31.5.tar.gz"
-  sha256 "f91035029b144538602e8577c25a043a8778ea0924fd9e2011c0e4697cee047a"
+  url "https://github.com/moonrepo/proto/archive/refs/tags/v0.32.0.tar.gz"
+  sha256 "baa7a65997f3bd935df92a6568cc8b04d291d46c801dec7290b75373e6ede95f"
   license "MIT"
   head "https://github.com/moonrepo/proto.git", branch: "master"
 
@@ -38,7 +38,7 @@ class Proto < Formula
       next if basename.to_s == "proto-shim"
 
       (libexec/"bin").install f
-      (bin/basename).write_env_script libexec/"bin"/basename, PROTO_INSTALL_DIR: opt_prefix/"bin"
+      (bin/basename).write_env_script libexec/"bin"/basename, PROTO_HOME: opt_prefix/"bin"
     end
   end
 
